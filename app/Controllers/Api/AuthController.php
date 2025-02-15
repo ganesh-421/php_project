b543217599b0c65eb9374238ca45f9d5785ed074
Ganesh Adhikari <ganesh149adhikari@gmail.com>
2025-02-15 16:35:59 +0545

    Fix: validator setups and some basic, widely used validation methods

--- app/Controllers/Api/AuthController.php
+++ app/Controllers/Api/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers\Api;
 
+use App\Core\Validator;
+use App\Models\User;
 use App\Repositories\AuthRepository;
 
 class AuthController
@@ -14,20 +16,33 @@ class AuthController
 
     public function login()
     {
+        $rules = [
+            'email' => "required|exists:user,email",
+            'password' => 'required'
+        ];
+        $data = [
+            'email' => $_POST['email'],
+            'password' => $_POST['password']
+        ];
+
+        $validator = new Validator($data, $rules, new User());
+
+        if(!$validator->validate()) {
+            $errors = json_encode($validator->errors());
+            var_dump($errors);
+        }
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $result = $this->repository->login($_POST['email'], $_POST['password']);
-            if($result)
-            {
-                header("Location: /dashboard");
-                exit;
-            } else {
-                $_SESSION['error'] = "Invalid Credentials";
-                header("Location: /login");
-                exit;
-            }
-        } else {
-            require_once __DIR__ . '/../Views/front/login.php';
-        }
+            // if($result)
+            // {
+            //     header("Location: /dashboard");
+            //     exit;
+            // } else {
+            //     $_SESSION['error'] = "Invalid Credentials";
+            //     header("Location: /login");
+            //     exit;
+            // }
+        } 
     }
 
     public function register()
