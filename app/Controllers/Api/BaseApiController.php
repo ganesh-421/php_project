b44c34e02d42e1381f13d269130d03fb746166c9
Ganesh Adhikari <ganesh149adhikari@gmail.com>
2025-02-15 16:44:28 +0545

    Fix: Json response if request expects json

---
+++ app/Controllers/Api/BaseApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers\Api;
+
+class BaseApiController
+{
+    public function sendSuccess($data, $message, $code)
+    {
+        return ;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -8,6 +8,14 @@ require_once __DIR__ . '/../app/Core/Autoloader.php';
 
 // routes
 if(!Request::expectJson()) 
+{
     require_once __DIR__ . '/../routes/web.php';
-else
+}
+else {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,PATCH");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     require_once __DIR__ . '/../routes/api.php';
+}
