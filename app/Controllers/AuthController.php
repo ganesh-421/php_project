b10fe7cfcd3b2af3be4dc9c7603130d5a868c140
Ganesh Adhikari <ganesh149adhikari@gmail.com>
2025-02-16 09:05:59 +0545

    Api controllers for logout and register

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Core\Validator;
+use App\Models\User;
 use App\Repositories\AuthRepository;
 
 class AuthController
@@ -14,6 +16,24 @@ class AuthController
 
     public function login()
     {
+        $rules = [
+            'email' => "required|email|exists:user,email",
+            'password' => 'required'
+        ];
+        $data = [
+            'email' => $_POST['email'],
+            'password' => $_POST['password']
+        ];
+
+        $validator = new Validator($data, $rules, new User());
+
+        if(!$validator->validate()) {
+            $errors = $validator->errors();
+            $_SESSION['errors'] = $errors;
+            header("Location: /login");
+            exit;
+        }
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $result = $this->repository->login($_POST['email'], $_POST['password']);
             if($result)
@@ -32,6 +52,37 @@ class AuthController
 
     public function register()
     {
+        $rules = [
+            "first_name" => 'required|min:3|max:255',
+            "last_name" => 'required|min:3|max:255',
+            "email" => 'required|email|unique:user,email',
+            "password" => 'required|min:8|max:15',
+            "phone" => 'required|min:10|max:10|unique:user,phone',
+            "dob" => 'required|before:today',
+            "gender" => 'required|in:m,f,o',
+            "address" => 'required|min:3|max:255',
+            "role" => 'required|in:super_admin,admin,artist',
+        ];
+        $data = [
+            "first_name" => $_POST['first_name'],
+            "last_name" => $_POST['last_name'],
+            "email" => $_POST['email'],
+            "password" => $_POST['password'],
+            "phone" => $_POST['phone'],
+            "dob" => $_POST['dob'],
+            "gender" => $_POST['gender'],
+            "address" => $_POST['address'],
+            "role" => $_POST['role'],
+            "created_at" => date('Y-m-d H:i:s'),
+            "updated_at" => date('Y-m-d H:i:s'),
+        ];
+        $validator = new Validator($data, $rules, (new User()));
+        if(!$validator->validate()) {
+            $errors = $validator->errors();
+            $_SESSION['errors'] = $errors;
+            header("Location: /register");
+            exit;
+        }
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $result = $this->repository->register($_POST);
             if($result) {
@@ -49,7 +100,7 @@ class AuthController
 
     public function logout()
     {
-        $this->repository->logout($_POST['email'], $_POST['password']);
+        $this->repository->logout();
         header("Location: /login");
     }
 }
